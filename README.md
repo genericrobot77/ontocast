06daaab530ea283b966604d4e5833fce6fec84bb
Alexander Belikov <abelikov@gmail.com>
2025-04-18 00:39:30 +0200

    up pyproj

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# mcp-ontokg
-
+# agentic-onto-triplecast
 
 ### Agent graph
 
 <!-- ![SVG Image](graph.png|200) -->
 [<img src="graph.png" width="400"/>](graph.png)
+
+
+## Running the server
+
+```shell
+uv run src/cli/serve.py --env-path path_openai_key --ontology-path ./data/ontologies --working-directory working_dir
+```
--- src/cli/plot_graph.py
+++ src/cli/plot_graph.py
@@ -41,69 +41,74 @@ frontmatter_config = {
     }
 }
 
-# Get the graph and save it as PNG
-app = create_agent_graph()
-graph = app.get_graph()
-mmd_data = graph.draw_mermaid(frontmatter_config=frontmatter_config)
+tools = {}
 
 
-# Save the PNG data to a file
-with open("graph.mmd", "w") as f:
-    f.write(mmd_data)
-mmd_data = mmd_data.replace("__start__", "START").replace("__end__", "END")
-# update_mermaid_graph_in_markdown("README.md", mmd_data)
+def main():
+    # Get the graph and save it as PNG
+    app = create_agent_graph(tools)
+    graph = app.get_graph()
+    mmd_data = graph.draw_mermaid(frontmatter_config=frontmatter_config)
 
-labels = {
-    "nodes": {"__end__": "END", "__start__": "START"},
-}
+    # Save the PNG data to a file
+    with open("graph.mmd", "w") as f:
+        f.write(mmd_data)
+    mmd_data = mmd_data.replace("__start__", "START").replace("__end__", "END")
+    # update_mermaid_graph_in_markdown("README.md", mmd_data)
+
+    labels = {
+        "nodes": {"__end__": "END", "__start__": "START"},
+    }
+
+    def tweak_draw(fname, ext):
+        fontname = "'Architects Daughter'"
+
+        subtle_green = "#a9cca9"
+        subtle_orange = "#ffdb99"
+        viz = pgv.AGraph(directed=True, nodesep=0.7, ranksep=0.5)
+        for node in graph.nodes:
+            viz.add_node(
+                node,
+                label=labels.get("nodes", {}).get(node, node),
+                style="filled",
+                fillcolor=subtle_green,
+                fontsize=12,
+                fontname=fontname,
+            )
+        for start, end, data, conditional in graph.edges:
+            label = str(data) if data is not None else ""
+            label = labels.get("edges", {}).get(label, label)
+            viz.add_edge(
+                start,
+                end,
+                label=label,
+                fontsize=10,
+                fontname=fontname,
+                style="dashed" if conditional else "solid",
+            )
+        if first := graph.first_node():
+            viz.get_node(first.id).attr.update(fillcolor=subtle_orange)
+        if last := graph.last_node():
+            viz.get_node(last.id).attr.update(fillcolor=subtle_orange)
+        if ext == "svg":
+            viz.draw(fname + ".svg", format="svg:cairo", prog="dot")
+        else:
+            viz.draw(fname + ".png", format="png", prog="dot", args="-Gdpi=300")
+
+    tweak_draw("graph", "svg")
+    tweak_draw("graph", "png")
+
+    # from langchain_core.runnables.graph import MermaidDrawMethod
+
+    # png_data = graph.draw_mermaid_png(
+    #     draw_method=MermaidDrawMethod.API,
+    #     frontmatter_config=frontmatter_config,
+    #     padding=20,
+    # )
+
+    # with open(output_path, "wb") as f:
+    #     f.write(png_data)
 
 
-def tweak_draw(fname, ext):
-    fontname = "'Architects Daughter'"
-
-    subtle_green = "#a9cca9"
-    subtle_orange = "#ffdb99"
-    viz = pgv.AGraph(directed=True, nodesep=0.7, ranksep=0.5)
-    for node in graph.nodes:
-        viz.add_node(
-            node,
-            label=labels.get("nodes", {}).get(node, node),
-            style="filled",
-            fillcolor=subtle_green,
-            fontsize=12,
-            fontname=fontname,
-        )
-    for start, end, data, conditional in graph.edges:
-        label = str(data) if data is not None else ""
-        label = labels.get("edges", {}).get(label, label)
-        viz.add_edge(
-            start,
-            end,
-            label=label,
-            fontsize=10,
-            fontname=fontname,
-            style="dashed" if conditional else "solid",
-        )
-    if first := graph.first_node():
-        viz.get_node(first.id).attr.update(fillcolor=subtle_orange)
-    if last := graph.last_node():
-        viz.get_node(last.id).attr.update(fillcolor=subtle_orange)
-    if ext == "svg":
-        viz.draw(fname + ".svg", format="svg:cairo", prog="dot")
-    else:
-        viz.draw(fname + ".png", format="png", prog="dot", args="-Gdpi=300")
-
-
-tweak_draw("graph", "svg")
-tweak_draw("graph", "png")
-
-# from langchain_core.runnables.graph import MermaidDrawMethod
-
-# png_data = graph.draw_mermaid_png(
-#     draw_method=MermaidDrawMethod.API,
-#     frontmatter_config=frontmatter_config,
-#     padding=20,
-# )
-
-# with open(output_path, "wb") as f:
-#     f.write(png_data)
+if __name__ == "__main__":
+    main()
